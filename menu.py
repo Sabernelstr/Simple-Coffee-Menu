934a7dc7221524ea8264373df9591f336ca4f2ea
Sabernelstr <danielfuster504@gmail.com>
2025-08-31 09:08:58 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,16 @@
+#Simple Coffe menu
+
+title = "Menu".upper()  #Menu header
+
+print(title.center(30, "="))
+
+#Menu contents
+print("Coffee".ljust(26, "-") + "$2".rjust(4)) 
+print("Muffin".ljust(26, "-") + "$1".rjust(4))   
+print("Cheesecake".ljust(26, "-") + "$3".rjust(4))   
+print("Checkers".ljust(26, "-") + "$4".rjust(4))
+print("Murriela".ljust(26, "-") + "$2".rjust(4))
+
+close = "THANK YOU"
+
+print(close.center(30, "="))
