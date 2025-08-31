1139455fb8364d9ede418193d4ce7e22db76f929
Sabernelstr <danielfuster504@gmail.com>
2025-08-31 09:11:56 +0100

    Update and rename menu.py to Coffee_Menu.py

--- Coffee_Menu.py
+++ Coffee_Menu.py
@@ -1,4 +1,4 @@
-#Simple Coffe menu
+#Simple Coffee menu
 
 title = "Menu".upper()  #Menu header
 
@@ -9,8 +9,8 @@ print("Coffee".ljust(26, "-") + "$2".rjust(4))
 print("Muffin".ljust(26, "-") + "$1".rjust(4))   
 print("Cheesecake".ljust(26, "-") + "$3".rjust(4))   
 print("Checkers".ljust(26, "-") + "$4".rjust(4))
-print("Murriela".ljust(26, "-") + "$2".rjust(4))
 
+#Menu footer
 close = "THANK YOU"
 
 print(close.center(30, "="))
